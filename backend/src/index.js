3404ab97f8ca7d46e128446f830b98999542378c
Muhammad Jinan <muhammadjinanillikal@gmail.com>
2026-02-08 17:10:39 +0530

    can support multiple frontend urls

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL.split(','),
   credentials: true
 }));
 app.use(express.json());
